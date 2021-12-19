d7c24f0b7dde3c31d92026d2f92b3e5af3317bc2
enyo <rongyy94job@gmail.com>
2021-12-20 00:20:41 +0800

    完成會員管理中心介面_增加性別

--- backend/member.php
+++ backend/member.php
@@ -12,7 +12,7 @@ $dsn="mysql:host=localhost;dbname=mypolling;charset=utf8";
 $pdo=new PDO($dsn,'root','');
 // $sql="SELECT * FROM `users`";
 
-$sql="select `users`.`id`,`account`,`name`,`birthday`,`email` from `users` where `id` > 0;";
+$sql="select `users`.`id`,`account`,`name`,`birthday`,`gender`,`email` from `users` where `id` > 0;";
 $users=$pdo->query($sql)->fetchALL();
 
 
@@ -21,6 +21,7 @@ echo "<tr>";
 echo "<td>id</td>";
 echo "<td>帳號</td>";
 echo "<td>姓名</td>";
+echo "<td>性別</td>";
 echo "<td>生日</td>";
 echo "<td>信箱</td>";
 echo "<td>操作</td>";
@@ -31,6 +32,7 @@ foreach($users as $user){
         echo "<td>{$user['id']}</td>";
         echo "<td>{$user['account']}</td>";
         echo "<td>{$user['name']}</td>";
+        echo "<td>{$user['gender']}</td>";
         echo "<td>{$user['birthday']}</td>";
         echo "<td>{$user['email']}</td>";
         echo "<td>";
